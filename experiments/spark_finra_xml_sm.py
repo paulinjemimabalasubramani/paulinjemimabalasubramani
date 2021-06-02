0a8be2000faf9ecc4725fc33671625dca51cd5f8
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2021-06-01 19:16:12 -0500

    recursive flattening - working solution

--- experiments/spark_finra_xml_sm.py
+++ experiments/spark_finra_xml_sm.py
@@ -26,6 +26,7 @@ from modules.mysession import MySession
 
 # %% Spark Libraries
 
+import pyspark
 from pyspark.sql.types import StructType, StructField, StringType, TimestampType, BooleanType, DoubleType, IntegerType, FloatType
 
 from pyspark.sql import functions as F
@@ -65,6 +66,7 @@ def recursive_unzip(folder_path:str, temp_path_folder:str=None, parent:str='', w
     if walk:
         for root, dirs, files in os.walk(folder_path):
             for file in files:
+                # Placeholder
                 if file.endswith('.zip'):
                     zip_files.append(os.path.join(root, file))
     else:
@@ -85,18 +87,84 @@ recursive_unzip(data_path_folder, temp_path_folder)
 
 # %%
 
-file_path = os.path.join(temp_path_folder, r'2021-05-16\7461_PostAppointmentsReport_2021-05-16.xml')
+file_path = os.path.join(temp_path_folder, r'2021-05-16\C:\Users\smammadov\packages\Temp\2021-05-16\7461_IndividualInformationReport_2021-05-16\7461_IndividualInformationReport_2021-05-16.iid')
 print(file_path)
 
 df = (ss.spark.read
-    .format("com.databricks.spark.xml")
-#    .option("rootTag", "PostAppointmentsReport")
-    .option("rowTag", "Criteria")
+    .format("xml")
+#    .option("rootTag", "?xml")
+    .option("rowTag", "?xml")
+    .option("inferSchema", 'true')
     .load(file_path)
 )
 
 
+df.printSchema()
+
+# %% Get Criteria
+
+def get_xml_criteria(df, criteria_str:str='Criteria'):
+    if df.count()==1 and criteria_str in df.columns:
+        dtype:str = [x[1] for x in df.dtypes if x[0] == criteria_str][0]
+        if dtype.startswith('struct'):
+            return df.select(col(criteria_str)).collect()[0][0].asDict()
+    return {}
+
+get_xml_criteria(df)
+
+
+# %% Critera-less DF
+
+
+dfc = df.select([col(c) for c in df.columns if c!='Criteria'])
+
+
+# %% Flatten XML
+
+
+def flatten_df(df) -> pyspark.sql.dataframe.DataFrame:
+    cols = []
+    nested = False
+
+    # to ensure not to have more than 1 expolosion in a table
+    expolode_flag = len([c[0] for c in df.dtypes if c[1].startswith('array')]) <= 1
+
+    for c in df.dtypes:
+        if c[1].startswith('struct'):
+            nested = True
+            if len(df.columns)>1:
+                struct_cols = df.select(c[0]+'.*').columns
+                cols.extend([col(c[0]+'.'+cc).alias(c[0]+'_'+cc) for cc in struct_cols])
+            else:
+                cols.append(c[0]+'.*')
+        elif c[1].startswith('array') and expolode_flag:
+            nested = True
+            cols.append(explode(c[0]).alias(c[0]))
+        else:
+            cols.append(c[0])
+
+    dfx = df.select(cols)
+    print('\n' ,dfx.columns)
+    if nested:
+        dfx = flatten_df(dfx)
+
+    return dfx
+
+
+dfy = flatten_df(dfc)
+
 # %%
 
-df.show()
+print(len(dfy.columns))
+dfy.printSchema()
+
+
+# %%
+
+dfy.limit(1).collect()
+
+
+# %%
+
+df.show(n=10, truncate=True)
 # %%
