4b30d7eeed6e3d613a550c397db11b8a89e3edab
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2021-08-25 13:29:04 -0500

    populated data_type_translation module

--- src/data_type_translation_lr_sql.py
+++ src/data_type_translation_lr_sql.py
@@ -23,14 +23,15 @@ from modules.config import is_pc
 from modules.spark_functions import create_spark, read_sql
 from modules.azure_functions import setup_spark_adls_gen2_connection, save_adls_gen2, tableinfo_name, read_adls_gen2, \
     get_azure_sp, file_format, tableinfo_container_name, to_storage_account_name, select_tableinfo_columns
-from modules.data_functions import execution_date, column_regex, metadata_DataTypeTranslation, metadata_MasterIngestList, \
-    partitionBy, partitionBy_value
-from modules.data_type_translation import join_master_ingest_list_sql_tables
+from modules.data_functions import execution_date, metadata_DataTypeTranslation, metadata_MasterIngestList, \
+    partitionBy
+from modules.data_type_translation import join_master_ingest_list_sql_tables, filter_columns_by_tables, join_tables_with_constraints, \
+    rename_columns, add_TargetDataType, add_precision
 
 
 from pyspark.sql import functions as F
 from pyspark.sql.functions import col, lit
-from pyspark.sql.types import IntegerType, StringType
+
 
 
 # %% Logging
@@ -127,143 +128,29 @@ if is_pc: sql_key_column_usage.show(5)
 
 
 
-# %% Join master_ingest_list with sql tables
+# %% Process Columns
 
-tables = join_master_ingest_list_sql_tables(master_ingest_list, sql_tables)
+# Join master_ingest_list with sql tables
+tables = join_master_ingest_list_sql_tables(master_ingest_list=master_ingest_list, sql_tables=sql_tables)
 
+# filter columns by selected tables
+columns = filter_columns_by_tables(sql_columns=sql_columns, tables=tables)
 
+# Join with table constraints and column usage
+columns = join_tables_with_constraints(columns=columns, sql_table_constraints=sql_table_constraints, sql_key_column_usage=sql_key_column_usage)
 
-# %% filter columns by selected tables
+# Rename Columns
+columns = rename_columns(columns=columns, storage_account_name=storage_account_name, created_datetime=created_datetime, modified_datetime=modified_datetime)
 
-@catch_error(logger)
-def filter_columns_by_tables(sql_columns, tables):
-    columns = tables.join(
-        sql_columns.alias('sql_columns'),
-        (tables.TABLE_NAME == sql_columns.TABLE_NAME) &
-        (tables.TABLE_SCHEMA == sql_columns.TABLE_SCHEMA) &
-        (tables.TABLE_CATALOG == sql_columns.TABLE_CATALOG),
-        how = 'left'
-    ).select('sql_columns.*').where(col('TABLE_NAME').isNotNull())
+# Add TargetDataType
+columns = add_TargetDataType(columns=columns, translation=translation)
 
-    if is_pc: columns.printSchema()
-    return columns
+# Add Precision
+columns = add_precision(columns=columns)
 
+# Select Relevant columns only
+columns = select_tableinfo_columns(columns=columns)
 
-
-columns = filter_columns_by_tables(sql_columns, tables)
-
-
-
-# %% Join with table constraints and column usage
-
-@catch_error(logger)
-def join_tables_with_constraints(columns, sql_table_constraints, sql_key_column_usage):
-    columns = columns.alias('columns').join(
-        sql_table_constraints,
-        (columns.TABLE_NAME == sql_table_constraints.TABLE_NAME) &
-        (columns.TABLE_SCHEMA == sql_table_constraints.TABLE_SCHEMA) &
-        (columns.TABLE_CATALOG == sql_table_constraints.TABLE_CATALOG) &
-        (sql_table_constraints.CONSTRAINT_TYPE == 'PRIMARY KEY'),
-        how = 'left'
-        ).select(
-            'columns.*', 
-            sql_table_constraints.CONSTRAINT_TYPE, 
-            sql_table_constraints.CONSTRAINT_NAME
-            )
-    
-    columns = columns.alias('columns').join(
-        sql_key_column_usage,
-        (columns.TABLE_NAME == sql_key_column_usage.TABLE_NAME) &
-        (columns.TABLE_SCHEMA == sql_key_column_usage.TABLE_SCHEMA) &
-        (columns.TABLE_CATALOG == sql_key_column_usage.TABLE_CATALOG) &
-        (columns.COLUMN_NAME == sql_key_column_usage.COLUMN_NAME) &
-        (columns.CONSTRAINT_NAME == sql_key_column_usage.CONSTRAINT_NAME)
-        ,
-        how = 'left'
-        ).select(
-            'columns.*',
-            sql_key_column_usage.COLUMN_NAME.alias('KEY_COLUMN_NAME')
-        )
-    
-    if is_pc: columns.printSchema()
-    return columns
-
-
-
-columns = join_tables_with_constraints(columns, sql_table_constraints, sql_key_column_usage)
-
-
-# %% Rename Columns
-
-@catch_error(logger)
-def rename_columns(columns):
-    column_map = {
-        'TABLE_CATALOG': 'SourceDatabase',
-        'TABLE_SCHEMA' : 'SourceSchema',
-        'TABLE_NAME'   : 'TableName',
-        'COLUMN_NAME'  : 'SourceColumnName',
-        'DATA_TYPE'    : 'SourceDataType',
-        'CHARACTER_MAXIMUM_LENGTH': 'SourceDataLength',
-        'NUMERIC_PRECISION': 'SourceDataPrecision',
-        'NUMERIC_SCALE': 'SourceDataScale',
-        'ORDINAL_POSITION': 'OrdinalPosition',
-    }
-
-    for key, val in column_map.items():
-        columns = columns.withColumnRenamed(key, val)
-
-    columns = columns.withColumn('IsNullable', F.when(F.upper(col('IS_NULLABLE'))=='YES', lit(1)).otherwise(lit(0)).cast(IntegerType()))
-    columns = columns.withColumn('KeyIndicator', F.when((F.upper(col('CONSTRAINT_TYPE'))=='PRIMARY KEY') & (col('SourceColumnName')==col('KEY_COLUMN_NAME')), lit(1)).otherwise(lit(0)).cast(IntegerType()))
-    columns = columns.withColumn('CleanType', col('SourceDataType'))
-    columns = columns.withColumn('StorageAccount', lit(storage_account_name).cast(StringType()))
-    columns = columns.withColumn('TargetColumnName', F.regexp_replace(col('SourceColumnName'), column_regex, '_'))
-    columns = columns.withColumn('IsActive', lit(1).cast(IntegerType()))
-    columns = columns.withColumn('CreatedDateTime', lit(created_datetime).cast(StringType()))
-    columns = columns.withColumn('ModifiedDateTime', lit(modified_datetime).cast(StringType()))
-    columns = columns.withColumn(partitionBy, lit(partitionBy_value).cast(StringType()))
-
-    if is_pc: columns.printSchema()
-    return columns
-
-
-
-columns = rename_columns(columns)
-
-
-# %% Add TargetDataType
-
-@catch_error(logger)
-def add_TargetDataType(columns, translation):
-    columns = columns.alias('columns').join(
-        translation,
-        columns.CleanType == translation.DataTypeFrom,
-        how = 'left'
-        ).select(
-            'columns.*', 
-            translation.DataTypeTo.alias('TargetDataType')
-        )
-
-    if is_pc: columns.printSchema()
-    return columns
-
-
-columns = add_TargetDataType(columns, translation)
-
-
-# %% Add Precision
-
-@catch_error(logger)
-def add_precision(columns):
-    columns = columns.withColumn('TargetDataType', F.when((col('TargetDataType').isin(['varchar'])) & (col('SourceDataLength')>0) & (col('SourceDataLength')<=255), F.concat(lit('varchar('), col('SourceDataLength'), lit(')'))).otherwise(col('TargetDataType')))
-    columns = columns.withColumn('TargetDataType', F.when((col('TargetDataType').isin(['decimal'])) & (col('SourceDataPrecision')>0), F.concat(lit('decimal('), col('SourceDataPrecision'), lit(','), col('SourceDataScale'), lit(')'))).otherwise(col('TargetDataType')))
-
-    if is_pc: columns.printSchema()
-    return columns
-    
-
-
-columns = add_precision(columns)
-columns = select_tableinfo_columns(columns)
 
 
 # %% Table Info to ADLS Gen 2
--- src/modules/data_type_translation.py
+++ src/modules/data_type_translation.py
@@ -7,9 +7,11 @@ Common Library for translating data types from source database to target databas
 
 from .common_functions import make_logging, catch_error
 from .config import is_pc
+from .data_functions import column_regex, partitionBy, partitionBy_value
 
-
+from pyspark.sql import functions as F
 from pyspark.sql.functions import col, lit
+from pyspark.sql.types import IntegerType, StringType
 
 
 # %% Logging
@@ -47,6 +49,123 @@ def join_master_ingest_list_sql_tables(master_ingest_list, sql_tables):
 
     return tables
 
+
+
+
+# %% filter columns by selected tables
+
+@catch_error(logger)
+def filter_columns_by_tables(sql_columns, tables):
+    columns = tables.join(
+        sql_columns.alias('sql_columns'),
+        (tables.TABLE_NAME == sql_columns.TABLE_NAME) &
+        (tables.TABLE_SCHEMA == sql_columns.TABLE_SCHEMA) &
+        (tables.TABLE_CATALOG == sql_columns.TABLE_CATALOG),
+        how = 'left'
+    ).select('sql_columns.*').where(col('TABLE_NAME').isNotNull())
+
+    if is_pc: columns.printSchema()
+    return columns
+
+
+
+# %% Join with table constraints and column usage
+
+@catch_error(logger)
+def join_tables_with_constraints(columns, sql_table_constraints, sql_key_column_usage):
+    columns = columns.alias('columns').join(
+        sql_table_constraints,
+        (columns.TABLE_NAME == sql_table_constraints.TABLE_NAME) &
+        (columns.TABLE_SCHEMA == sql_table_constraints.TABLE_SCHEMA) &
+        (columns.TABLE_CATALOG == sql_table_constraints.TABLE_CATALOG) &
+        (sql_table_constraints.CONSTRAINT_TYPE == 'PRIMARY KEY'),
+        how = 'left'
+        ).select(
+            'columns.*', 
+            sql_table_constraints.CONSTRAINT_TYPE, 
+            sql_table_constraints.CONSTRAINT_NAME
+            )
+    
+    columns = columns.alias('columns').join(
+        sql_key_column_usage,
+        (columns.TABLE_NAME == sql_key_column_usage.TABLE_NAME) &
+        (columns.TABLE_SCHEMA == sql_key_column_usage.TABLE_SCHEMA) &
+        (columns.TABLE_CATALOG == sql_key_column_usage.TABLE_CATALOG) &
+        (columns.COLUMN_NAME == sql_key_column_usage.COLUMN_NAME) &
+        (columns.CONSTRAINT_NAME == sql_key_column_usage.CONSTRAINT_NAME)
+        ,
+        how = 'left'
+        ).select(
+            'columns.*',
+            sql_key_column_usage.COLUMN_NAME.alias('KEY_COLUMN_NAME')
+        )
+    
+    if is_pc: columns.printSchema()
+    return columns
+
+
+
+# %% Rename Columns
+
+@catch_error(logger)
+def rename_columns(columns, storage_account_name:str, created_datetime:str, modified_datetime:str):
+    column_map = {
+        'TABLE_CATALOG': 'SourceDatabase',
+        'TABLE_SCHEMA' : 'SourceSchema',
+        'TABLE_NAME'   : 'TableName',
+        'COLUMN_NAME'  : 'SourceColumnName',
+        'DATA_TYPE'    : 'SourceDataType',
+        'CHARACTER_MAXIMUM_LENGTH': 'SourceDataLength',
+        'NUMERIC_PRECISION': 'SourceDataPrecision',
+        'NUMERIC_SCALE': 'SourceDataScale',
+        'ORDINAL_POSITION': 'OrdinalPosition',
+    }
+
+    for key, val in column_map.items():
+        columns = columns.withColumnRenamed(key, val)
+
+    columns = columns.withColumn('IsNullable', F.when(F.upper(col('IS_NULLABLE'))=='YES', lit(1)).otherwise(lit(0)).cast(IntegerType()))
+    columns = columns.withColumn('KeyIndicator', F.when((F.upper(col('CONSTRAINT_TYPE'))=='PRIMARY KEY') & (col('SourceColumnName')==col('KEY_COLUMN_NAME')), lit(1)).otherwise(lit(0)).cast(IntegerType()))
+    columns = columns.withColumn('CleanType', col('SourceDataType'))
+    columns = columns.withColumn('StorageAccount', lit(storage_account_name).cast(StringType()))
+    columns = columns.withColumn('TargetColumnName', F.regexp_replace(col('SourceColumnName'), column_regex, '_'))
+    columns = columns.withColumn('IsActive', lit(1).cast(IntegerType()))
+    columns = columns.withColumn('CreatedDateTime', lit(created_datetime).cast(StringType()))
+    columns = columns.withColumn('ModifiedDateTime', lit(modified_datetime).cast(StringType()))
+    columns = columns.withColumn(partitionBy, lit(partitionBy_value).cast(StringType()))
+
+    if is_pc: columns.printSchema()
+    return columns
+
+
+
+# %% Add TargetDataType
+
+@catch_error(logger)
+def add_TargetDataType(columns, translation):
+    columns = columns.alias('columns').join(
+        translation,
+        columns.CleanType == translation.DataTypeFrom,
+        how = 'left'
+        ).select(
+            'columns.*', 
+            translation.DataTypeTo.alias('TargetDataType')
+        )
+
+    if is_pc: columns.printSchema()
+    return columns
+
+
+
+# %% Add Precision
+
+@catch_error(logger)
+def add_precision(columns):
+    columns = columns.withColumn('TargetDataType', F.when((col('TargetDataType').isin(['varchar'])) & (col('SourceDataLength')>0) & (col('SourceDataLength')<=255), F.concat(lit('varchar('), col('SourceDataLength'), lit(')'))).otherwise(col('TargetDataType')))
+    columns = columns.withColumn('TargetDataType', F.when((col('TargetDataType').isin(['decimal'])) & (col('SourceDataPrecision')>0), F.concat(lit('decimal('), col('SourceDataPrecision'), lit(','), col('SourceDataScale'), lit(')'))).otherwise(col('TargetDataType')))
+
+    if is_pc: columns.printSchema()
+    return columns
 
 
 
