3b02c85dbf27626e1716f60daa499f8adf677ffb
Fields, Jared <jared.fields@advisorgroup.com>
2021-09-02 04:43:25 +0000

    Updated azure_functions.py

--- src/modules/azure_functions.py
+++ src/modules/azure_functions.py
@@ -419,7 +419,7 @@ def post_data(customer_id, shared_key, body, log_type):
     method = 'POST'
     content_type = 'application/json'
     resource = '/api/logs'
-    rfc1123date = datetime.utcnow().strftime('%a, %d %B %Y %H:%M:%S GMT')
+    rfc1123date = datetime.now().strftime('%a, %d %b %Y %H:%M:%S GMT')
     print(f'{rfc1123date}')
     content_length = len(body)
     signature = build_signature(customer_id, shared_key, rfc1123date, content_length, method, content_type, resource)
