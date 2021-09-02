c629f2db13b968d49559b3dfee683b4b8cc3d741
Fields, Jared <jared.fields@advisorgroup.com>
2021-09-02 14:35:04 +0000

    Updated azure_functions.py

--- src/modules/azure_functions.py
+++ src/modules/azure_functions.py
@@ -181,7 +181,7 @@ def log_saved_data(
         partitionBy:str=None,
         file_format:str=file_format):
     timestamp = datetime.now()
-    log_data = {"TimeGenerated": str(datestamp), "Table": table_to_save,  "Storage_Account": storage_account_name, "Container": container_name, "Folder": container_folder, "Table": table_name, "Partitioning": partitionBy, "Format": file_format}
+    log_data = {"TimeGenerated": str(timestamp), "Table": table_to_save,  "Storage_Account": storage_account_name, "Container": container_name, "Folder": container_folder, "Table": table_name, "Partitioning": partitionBy, "Format": file_format}
     tenant_id,customer_id,shared_key = get_azure_sp("loganalytics")
     log_type = "AirlfowSavedTables"
     post_data(customer_id, shared_key, log_data, log_type)
