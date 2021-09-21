693bbbc3f2ab63b511f05456d971708bfd855ea6
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2021-09-21 16:57:44 -0500

    Preprocess Schema

--- src/pershing_accf.py
+++ src/pershing_accf.py
@@ -23,16 +23,15 @@ sys.parent_name = os.path.basename(__file__)
 sys.domain_name = 'client_and_account'
 sys.domain_abbr = 'CA'
 
-
 # Add 'modules' path to the system environment - adjust or remove this as necessary
 sys.path.append(os.path.realpath(os.path.dirname(__file__)+'/../../src'))
 sys.path.append(os.path.realpath(os.path.dirname(__file__)+'/../src'))
-
 
 from modules.common_functions import data_settings, get_secrets, logger, mark_execution_end, config_path, is_pc
 from modules.spark_functions import create_spark, read_csv, read_text, column_regex
 from modules.azure_functions import setup_spark_adls_gen2_connection, read_tableinfo_rows, default_storage_account_name, tableinfo_name
 
+from pprint import pprint
 
 from pyspark.sql import functions as F
 from pyspark.sql.functions import col, lit
@@ -42,17 +41,8 @@ from pyspark.sql.types import IntegerType
 
 # %% Parameters
 
-ingest_from_files_flag = True
-
-sql_server = ''
-sql_key_vault_account = sql_server
-
 storage_account_name = default_storage_account_name
-
 tableinfo_source = 'ACCF'
-sql_database = tableinfo_source # TABLE_CATALOG
-
-data_type_translation_id = 'sqlserver_snowflake'
 
 data_path_folder = data_settings.get_value(attr_name=f'data_path_{tableinfo_source}', default_value=os.path.join(data_settings.data_path, tableinfo_source))
 
@@ -61,8 +51,13 @@ logger.info({
     'data_path_folder': data_path_folder,
 })
 
+header_str = 'BOF      PERSHING '
+trailer_str = 'EOF      PERSHING '
+transaction_code = 'CI'
+
 
 # https://stackoverflow.com/questions/41944689/pyspark-parse-fixed-width-text-file
+
 
 
 # %% Create Session
@@ -74,27 +69,22 @@ spark = create_spark()
 schema_folder_path = os.path.join(config_path, 'pershing_schema')
 schema_file_path_accf = os.path.join(schema_folder_path, 'customer_account_information_acct_accf.csv')
 
-schema_accf = read_csv(spark=spark, file_path=schema_file_path_accf)
+schema = read_csv(spark=spark, file_path=schema_file_path_accf)
 
 
 # %% Preprocess Schema
 
-schema_accf = schema_accf.withColumn('field_name', F.lower(F.regexp_replace(F.trim(col('field_name')), column_regex, '_')))
-schema_accf = schema_accf.withColumn('record_name', F.upper(col('record_name')))
-schema_accf = schema_accf.withColumn('conditional_changes', F.upper(col('conditional_changes')))
-schema_accf = schema_accf.withColumn('position_start', F.split(col('position'), pattern='-').getItem(0).cast(IntegerType()))
-schema_accf = schema_accf.withColumn('position_end', F.split(col('position'), pattern='-').getItem(1).cast(IntegerType()))
-schema_accf = schema_accf.where(col('field_name')!='not_used')
+schema = schema.withColumn('field_name', F.lower(F.regexp_replace(F.trim(col('field_name')), column_regex, '_')))
+schema = schema.withColumn('record_name', F.upper(col('record_name')))
+schema = schema.withColumn('conditional_changes', F.upper(col('conditional_changes')))
+schema = schema.withColumn('position_start', F.split(col('position'), pattern='-').getItem(0).cast(IntegerType()))
+schema = schema.withColumn('position_end', F.split(col('position'), pattern='-').getItem(1).cast(IntegerType()))
+schema = schema.withColumn('length', col('position_end') - col('position_start') + lit(1))
+schema = schema.where(col('field_name')!='not_used')
 
 
-if is_pc: schema_accf.show(20, True)
+if is_pc: schema.show(20, True)
 
-
-
-# %%
-
-record_names = schema_accf.select('record_name').distinct().collect()
-record_names = [x['record_name'] for x in record_names]
 
 
 # %% Read Text File
@@ -103,24 +93,38 @@ file_name = '4CCF.4CCF'
 
 text_file = read_text(spark=spark, file_path=os.path.join(data_path_folder, file_name))
 
-cv = col('value')
+
+# %%
+
+tables = dict()
+
+cv = col('value').substr
+
+record_names = schema.select('record_name').distinct().collect()
+record_names = [x['record_name'] for x in record_names]
+if is_pc: pprint(record_names)
+
+for record_name in record_names:
+    if is_pc: pprint(f'Record Name: {record_name}')
+    if record_name == 'HEADER':
+        table = text_file.where(cv(1, len(header_str))==lit(header_str))
+    elif record_name == 'TRAILER':
+        table = text_file.where(cv(1, len(trailer_str))==lit(trailer_str))
+    else:
+        table = text_file.where(
+            (cv(1, len(transaction_code))==lit(transaction_code)) &
+            (cv(3, 1)==lit(record_name))
+            )
+
+    for schema_row in schema.where(col('record_name')==lit(record_name)).rdd.toLocalIterator():
+        schema_dict = schema_row.asDict()
+        table = table.withColumn(schema_dict['field_name'], cv(schema_dict['position_start'], schema_dict['length']))
+
+    if is_pc: table.show(5)
+    tables[record_name] = table
 
 
 
-bof_pershing = 'BOF      PERSHING '
-eof_pershing = 'EOF      PERSHING '
-
-header = text_file.where(cv.substr(1, len(bof_pershing))==lit(bof_pershing))
-trailer = text_file.where(cv.substr(1, len(eof_pershing))==lit(eof_pershing))
-
-text_file = text_file.where(
-    (cv.substr(1, len(bof_pershing))!=lit(bof_pershing)) &
-    (cv.substr(1, len(eof_pershing))!=lit(eof_pershing))
-    )
-
-header.show()
-trailer.show()
-text_file.show()
 
 
 
