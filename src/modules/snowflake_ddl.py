ff3d02ef79e8f767e57634341bc0f54060b4c9f0
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2021-11-18 10:01:47 -0600

    Add src.ELT_STREAM_ACTION to top_slice

--- src/modules/snowflake_ddl.py
+++ src/modules/snowflake_ddl.py
@@ -669,7 +669,7 @@ SELECT
   ,{column_list_with_alias}
   ,{hash_columns} AS {wid.hash_column_name}
   ,{wid.src_alias}.{wid.elt_stream_action_alias}
-  ,ROW_NUMBER() OVER (PARTITION BY {pk_column_with_alias} ORDER BY {pk_column_with_alias}, {wid.src_alias}.{EXECUTION_DATE_str} DESC) AS top_slice
+  ,ROW_NUMBER() OVER (PARTITION BY {pk_column_with_alias}, {wid.src_alias}.{wid.elt_stream_action_alias} ORDER BY {pk_column_with_alias}, {wid.src_alias}.{wid.elt_stream_action_alias}, {wid.src_alias}.{EXECUTION_DATE_str} DESC) AS top_slice
 FROM {SCHEMA_NAME}.{wid.view_prefix}{TABLE_NAME}{wid.variant_label}{wid.stream_suffix} {wid.src_alias}
 )
 WHERE top_slice = 1;
