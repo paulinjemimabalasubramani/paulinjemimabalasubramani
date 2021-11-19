60e9faf14d3435d251c369df428a76a75bfb6dcd
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2021-11-19 12:32:02 -0600

    complete extract_albridge_file_meta()

--- src/migrate_albridge.py
+++ src/migrate_albridge.py
@@ -13,7 +13,7 @@ http://10.128.25.82:8282/
 
 # %% Import Libraries
 
-import os, sys, re
+import os, sys, re, csv
 sys.parent_name = os.path.basename(__file__)
 sys.domain_name = 'customer_assets'
 sys.domain_abbr = 'ASSETS'
@@ -52,17 +52,12 @@ tableinfo_source = 'ALBRIDGE'
 
 data_path_folder = data_settings.get_value(attr_name=f'data_path_{tableinfo_source}', default_value=os.path.join(data_settings.data_path, tableinfo_source))
 schema_folder_path = os.path.join(config_path, 'albridge_schema')
+schema_file_name = 'albridge_schema.csv'
 
-table_special_records = defaultdict(dict)
-
-pershing_strftime = r'%m/%d/%Y %H:%M:%S' # http://strftime.org/
 date_start = datetime.strptime(data_settings.file_history_start_date[tableinfo_source], r'%Y-%m-%d')
 
-header_str = 'BOF      PERSHING '
-trailer_str = 'EOF      PERSHING '
 schema_header_str = 'HEADER'
 schema_trailer_str = 'TRAILER'
-groupBy = ['account_number']
 
 date_column_name = 'run_datetime'
 key_column_names = get_key_column_names(date_column_name=date_column_name)
@@ -89,375 +84,146 @@ firms = get_firms_with_crd(spark=spark, tableinfo_source=firms_source)
 
 if is_pc: pprint(firms)
 
+# temporarily change firm crd number to firm name:
+for i in range(len(firms)):
+    firms[i]['crd_number'] = firms[i]['firm_name']
+
 
 
 # %% get and pre-process schema
 
 @catch_error(logger)
-def get_pershing_schema(spark, schema_file_name:str):
+def get_albridge_schema():
     """
     Read and Pre-process the schema table to make it code-friendly
     """
     schema_file_path = os.path.join(schema_folder_path, schema_file_name)
-    if not os.path.isfile(schema_file_path):
-        logger.warning(f'Schema file is not found: {schema_file_path}')
+
+    schema = defaultdict(list)
+    albridge_file_types = dict()
+
+    with open(schema_file_path, newline='', encoding='utf-8-sig') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            file_types = row['file_type'].upper().split(',')
+            is_primary_key = row['is_primary_key'].strip().upper() == 'Y'
+
+            column_name = row['column_name'].strip().lower()
+            if column_name in ['', 'n/a', 'none', '_', '__', 'na', 'null', '-', '.']:
+                column_name = 'unused'
+
+            file_type_desc = row['file_type_desc'].strip()
+
+            for file_type in file_types:
+                ftype = file_type.strip()
+                schema[ftype].append({
+                    'is_primary_key': is_primary_key,
+                    'column_name': column_name,
+                    })
+                if ftype not in ['HEADER', 'TRAILER']:
+                    albridge_file_types[ftype] = file_type_desc
+
+    return schema, albridge_file_types
+
+
+
+schema, albridge_file_types = get_albridge_schema()
+
+
+
+# %% Extract Meta Data from Albridge file
+
+@catch_error(logger)
+def extract_albridge_file_meta(file_path:str, firm_crd_number:str, cloud_file_history):
+    """
+    Extract Meta Data from Albridge file (reading 1st line (header metadata) from inside the file)
+    """
+    convert_yyyymmdd = lambda s: f'{s[:4]}-{s[4:6]}-{s[6:]}'
+    convert_hhmmss = lambda s: f'{s[:2]}:{s[2:4]}:{s[4:]}'
+    strftime = r'%Y-%m-%d %H:%M:%S' # http://strftime.org/
+
+    file_name = os.path.basename(file_path)
+    file_name_noext, file_ext = os.path.splitext(file_name)
+    file_name_noext = file_name_noext.upper()
+
+    if file_ext.lower() not in ['.txt']:
+        logger.warning(f'Not a .txt file: {file_path}')
         return
 
-    schema = read_csv(spark=spark, file_path=schema_file_path)
-    schema = remove_column_spaces(schema)
-    schema = schema.withColumn('field_name', F.lower(F.regexp_replace(F.trim(col('field_name')), column_regex, '_')))
-    schema = schema.withColumn('record_name', F.upper(F.trim(col('record_name'))))
-    schema = schema.withColumn('conditional_changes', F.upper(F.trim(col('conditional_changes'))))
-    schema = schema.withColumn('position', F.trim(col('position')))
+    if not 13<=len(file_name_noext)<=16 \
+        or not file_name_noext[0].isalpha() \
+        or not file_name_noext[2:].isdigit() \
+        or not (file_name_noext[:1] in albridge_file_types or file_name_noext[:2] in albridge_file_types):
+        logger.warning(f'Not a valid Albirdge Replication/Export file name: {file_path}')
+        return
 
-    schema = schema.where(
-        (col('field_name').isNotNull()) & (~col('field_name').isin(['', 'not_used', '_', '__', 'n_a', 'na', 'none', 'null', 'value'])) &
-        (col('position').contains('-')) &
-        (col('record_name').isNotNull())
-        )
+    sequence_number = file_name_noext[-2:]
+    file_date = convert_yyyymmdd(file_name_noext[-10:-2])
+    file_name_prefix = file_name_noext[:-10]
 
-    schema = schema.withColumn('position_start', F.split(col('position'), pattern='-').getItem(0).cast(IntegerType()))
-    schema = schema.withColumn('position_end', F.split(col('position'), pattern='-').getItem(1).cast(IntegerType()))
-    schema = schema.withColumn('length', col('position_end') - col('position_start') + lit(1))
-
-    schema.persist(StorageLevel.MEMORY_AND_DISK)
-    return schema
-
-
-
-# %% Get Header Schema
-
-@catch_error(logger)
-def get_header_schema():
-    """
-    Get Schema for Header section of Pershing files
-    """
-    selected_fields = ['position_start', 'position_end', 'length']
-
-    schema = get_pershing_schema(spark=spark, schema_file_name='customer_acct_info.csv')
-    if not schema: raise Exception('Main Schema file is not found!')
-
-    header_schema = schema.where(col('record_name')==lit(schema_header_str)).select(['field_name', *selected_fields]).collect()
-    header_schema = {r['field_name']: {field_name: r[field_name] for field_name in selected_fields} for r in header_schema}
-    header_schema['form_name'] = header_schema.pop('customer_acct_info')
-    return header_schema
-
-
-
-header_schema = get_header_schema()
-
-
-
-# %% Extract field from a fixed width table
-
-@catch_error(logger)
-def extract_field_from_fwt(table, field_name:str, position_start:int, length:int):
-    """
-    Extract a field from a fixed width table
-    """
-    cv = col('value').substr
-    return table.withColumn(field_name, F.regexp_replace(F.trim(cv(position_start, length)), ' +', ' '))
-
-
-
-# %% Add Schema fields to table
-
-@catch_error(logger)
-def add_schema_fields_to_table(tables, table, schema, record_name:str, conditional_changes:str=''):
-    """
-    Add fileds defined in Schema to the table and collect in "tables" list bases on record_name and conditional_changes
-    """
-    if record_name == schema_header_str:
-        for field_name, pos in header_schema.items():
-            table = extract_field_from_fwt(table=table, field_name=field_name, position_start=pos['position_start'], length=pos['length'])
+    if len(file_name_prefix)==3:
+        if file_name_prefix[:1] in albridge_file_types and file_name_prefix[1:].isdigit():
+            file_type = file_name_prefix[:1]
+            fin_inst_id = file_name_prefix[1:]
+        elif file_name_prefix[:2] in albridge_file_types and file_name_prefix[2:].isdigit():
+            file_type = file_name_prefix[:2]
+            fin_inst_id = file_name_prefix[2:]
+        else:
+            logger.warning(f'Not a valid Albirdge Replication/Export file name prefix: {file_path}')
+            return
     else:
-        schema = schema.where(col('record_name')==lit(record_name))
-        if schema.rdd.isEmpty(): return
-
-        for schema_row in schema.rdd.toLocalIterator():
-            schema_dict = schema_row.asDict()
-            table = extract_field_from_fwt(table=table, field_name=schema_dict['field_name'], position_start=schema_dict['position_start'], length=schema_dict['length'])
-
-    table = table.drop('value')
-    tables[(record_name, conditional_changes)] = table
-
-
-
-# %% Find Field Position and Length
-
-@catch_error(logger)
-def find_field_position(schema, record_name:str, field_name:str):
-    """
-    Find field Position and Length from schema for a given record_name and field_name
-    """
-    field_schema = schema.where(
-        (col('record_name')==lit(record_name)) &
-        (col('field_name')==lit(field_name))
-        ).select(['position_start', 'length']).collect()[0]
-
-    position_start = field_schema['position_start']
-    length = field_schema['length']
-
-    if is_pc: pprint({
-        'record_name': record_name,
-        'field_name': field_name,
-        'position_start': position_start,
-        'length': length,
-        })
-
-    return position_start, length
-
-
-
-# %% Get Distinct Field values
-
-@catch_error(logger)
-def get_dictinct_field_values(table, schema, record_name:str, field_name:str):
-    """
-    Get Distinct Field values from table for given record_name and field_name
-    """
-    position_start, length = find_field_position(schema=schema, record_name=record_name, field_name=field_name)
-    table = extract_field_from_fwt(table=table, field_name=field_name, position_start=position_start, length=length)
-
-    field_values = collect_column(table=table, column_name=field_name, distinct=True)
-    return table, field_values
-
-
-
-# %% Add Sub-tables to table
-
-@catch_error(logger)
-def add_sub_tables_to_table(tables, schema, sub_tables, record_name:str):
-    """
-    Add sub-tables from special_records (with conditional_changes) to the tables list
-    """
-    for conditional_changes, sub_table in sub_tables.items():
-        #if sub_table.rdd.isEmpty(): continue
-
-        filter_schema = schema.where(
-            (col('record_name')==lit(record_name)) & (
-                (col('conditional_changes').contains(conditional_changes.upper())) |
-                (col('conditional_changes').isNull()) |
-                (col('conditional_changes')==lit(''))
-                )
-            )
-
-        add_schema_fields_to_table(tables=tables, table=sub_table, schema=filter_schema, record_name=record_name, conditional_changes=conditional_changes)
-
-
-
-# %% generate tables from fixed width table file
-
-@catch_error(logger)
-def generate_tables_from_fwt(
-        text_file,
-        schema,
-        table_name:str,
-        header_str:str = header_str,
-        trailer_str:str = trailer_str,
-        transaction_code:str = 'CI',
-        ):
-    """
-    Generate list of sub-tables from a fixed width table file based on record_name and conditional_changes
-    """
-    tables = dict()
-    cv = col('value').substr
-
-    special_records = table_special_records[table_name]
-    record_names = collect_column(table=schema, column_name='record_name', distinct=True)
-
-    for record_name in record_names:
-        if is_pc: pprint(f'Record Name: {record_name}')
-
-        if record_name == schema_header_str:
-            table = text_file.where(cv(1, len(header_str))==lit(header_str))
-        elif record_name == schema_trailer_str:
-            table = text_file.where(cv(1, len(trailer_str))==lit(trailer_str))
+        if file_name_prefix[:2] in albridge_file_types and file_name_prefix[2:].isdigit():
+            file_type = file_name_prefix[:2]
+            fin_inst_id = file_name_prefix[2:]
+        elif file_name_prefix[:1] in albridge_file_types and file_name_prefix[1:].isdigit():
+            file_type = file_name_prefix[:1]
+            fin_inst_id = file_name_prefix[1:]
         else:
-            table = text_file.where(
-                (cv(1, len(transaction_code))==lit(transaction_code)) &
-                (cv(3, 1)==lit(record_name))
-                )
-
-        #if table.rdd.isEmpty(): continue
-
-        if record_name in special_records:
-            sub_tables = special_records[record_name](table=table, schema=schema, record_name=record_name)
-            add_sub_tables_to_table(tables=tables, schema=schema, sub_tables=sub_tables, record_name=record_name)
-        else:
-            add_schema_fields_to_table(tables=tables, table=table, schema=schema, record_name=record_name)
-
-    return tables
-
-
-
-# %% Union Tables per Record
-
-@catch_error(logger)
-def union_tables_per_record(tables):
-    """
-    Union Tables per Record (union conditional_changes)
-    """
-    table_per_record = dict()
-    for key, table in tables.items():
-        record_name = key[0]
-        if record_name in table_per_record:
-            table_per_record[record_name] = table_per_record[record_name].unionByName(table, allowMissingColumns=True)
-        else:
-            table_per_record[record_name] = table
-
-    return table_per_record
-
-
-
-# %% Combine Rows into Array
-
-@catch_error(logger)
-def combine_rows_into_array(tables, groupBy:list):
-    """
-    Utility function to combine rows into Array (so that to have one column of json data per record name in the final main table)
-    """
-    for record_name in tables.keys():
-        if record_name not in [schema_header_str, schema_trailer_str]:
-            tables[record_name] = (tables[record_name]
-                .withColumn('all_columns', F.struct(tables[record_name].columns))
-                .groupBy(groupBy)
-                .agg(F.collect_list('all_columns').alias(f'record_{record_name}'))
-                )
-
-    return tables
-
-
-
-# %% Join All Tables
-
-@catch_error(logger)
-def join_all_tables(tables, groupBy:list):
-    """
-    Join all sub-tables that were split by record_name and conditional_changes
-    """
-    joined_tables = None
-
-    for record_name, table in tables.items():
-        if record_name not in [schema_header_str, schema_trailer_str]:
-            if joined_tables:
-                joined_tables = joined_tables.join(table, on=groupBy, how='full')
-            else:
-                joined_tables = table
-
-    header = tables[schema_header_str].collect()[0]
-    header_map = ['date_of_data', 'remote_id', 'run_date', 'run_time', 'refreshed_updated']
-    for column_name in header_map:
-        joined_tables = joined_tables.withColumn(column_name, lit(header[column_name]))
-
-    joined_tables.persist(StorageLevel.MEMORY_AND_DISK)
-    return joined_tables
-
-
-
-# %% Create Table from Fixed-With Table File
-
-@catch_error(logger)
-def create_table_from_fwt_file(firm_path_folder:str, file_name:str, schema_file_name:str, table_name:str, groupBy:list):
-    """
-    Create Table from Fixed-With Table File. Convery FWT to nested Spark table.
-    """
-    data_file_path = os.path.join(firm_path_folder, file_name)
-
-    logger.info({
-        'action': 'generate_tables_from_fwt_file',
-        'data_file_path': data_file_path,
-        'schema_file_name': schema_file_name,
-    })
-
-    schema = get_pershing_schema(spark=spark, schema_file_name=schema_file_name)
-    if not schema: return
-
-    text_file = read_text(spark=spark, file_path=data_file_path)
-
-    tables = generate_tables_from_fwt(text_file=text_file, schema=schema, table_name=table_name)
-    tables = union_tables_per_record(tables=tables)
-    tables = combine_rows_into_array(tables=tables, groupBy=groupBy)
-
-    table = join_all_tables(tables=tables, groupBy=groupBy)
-    return table
-
-
-
-# %% Process customer_acct_info Record A
-
-@catch_error(logger)
-def process_record_A_customer_acct_info(table, schema, record_name):
-    """
-    Special Record processing for customer_acct_info Record A - because of it has conditional_changes
-    """
-    if record_name != 'A': return
-
-    registration_type_field_name = 'registration_type'
-    table, registration_types = get_dictinct_field_values(table=table, schema=schema, record_name=record_name, field_name=registration_type_field_name)
-
-    sub_tables = {registration_type:table.where(col(registration_type_field_name)==lit(registration_type)) for registration_type in registration_types}
-    return sub_tables
-
-
-table_special_records['customer_acct_info']['A'] = process_record_A_customer_acct_info
-
-
-
-# %% Process customer_acct_info Record C
-
-@catch_error(logger)
-def process_record_C_customer_acct_info(table, schema, record_name):
-    """
-    Special Record processing for customer_acct_info Record C - because of it has conditional_changes
-    """
-    if record_name != 'C': return
-
-    country_field_name = 'country_code_1'
-    table, countries = get_dictinct_field_values(table=table, schema=schema, record_name=record_name, field_name=country_field_name)
-
-    USCA_codes = ['US','CA']
-
-    sub_tables = {
-        'US or Canada addresses only': table.where(col(country_field_name).isin(USCA_codes)),
-        'Non-US or Canada addresses only': table.where(~col(country_field_name).isin(USCA_codes)),
-    }
-    return sub_tables
-
-
-table_special_records['customer_acct_info']['C'] = process_record_C_customer_acct_info
-
-
-
-# %% Extract Meta Data from Pershing FWT file
-
-@catch_error(logger)
-def extract_pershing_file_meta(file_path:str, firm_crd_number:str, cloud_file_history):
-    """
-    Extract Meta Data from Pershing FWT file (reading 1st line (header metadata) from inside the file)
-    """
-    with open(file=file_path, mode='rt') as f:
-        HEADER = f.readline()
-
-    if HEADER[:len(header_str)] != header_str: 
-        logger.warning(f'Not a Pershing file: {file_path}')
-        return
+            logger.warning(f'Not a valid Albirdge Replication/Export file name prefix: {file_path}')
+            return
 
     file_meta = {
-        'file_name': os.path.basename(file_path),
+        'file_name': file_name,
         'file_path': file_path,
         'folder_path': os.path.dirname(file_path),
         FirmCRDNumber: firm_crd_number,
+        'sequence_number': sequence_number,
+        'file_date': file_date,
+        'file_type': file_type,
+        'file_type_desc': albridge_file_types[file_type],
+        'fin_inst_id': fin_inst_id,
     }
 
-    for field_name, pos in header_schema.items():
-        file_meta[field_name] = re.sub(' +', ' ', HEADER[pos['position_start']-1: pos['position_end']].strip())
+    with open(file=file_path, mode='rt') as f:
+        HEADER = f.readline()
 
-    file_meta['table_name'] = re.sub(' ', '_', file_meta['form_name'].lower())
-    file_meta['schema_file_name'] = file_meta['table_name'] + '.csv'
-    file_meta['is_full_load'] = file_meta['refreshed_updated'].upper() == 'REFRESHED'
-    file_meta[date_column_name] = datetime.strptime(' '.join([file_meta['run_date'], file_meta['run_time']]), pershing_strftime)
+    if HEADER[:2] != 'H|':
+        logger.warning(f'Header is not found in 1st line inside the file: {file_path}')
+        return
+
+    HEADER = HEADER.split(sep='|')
+    header_schema = [c['column_name'].lower() for c in schema['HEADER']]
+
+    for i in range(1, len(header_schema)):
+        file_meta[header_schema[i]] = HEADER[i]
+
+    file_meta['table_name'] = file_meta['file_desc'][:-4]
+    file_meta['eff_date'] = convert_yyyymmdd(file_meta['eff_date'])
+    file_meta['run_date'] = convert_yyyymmdd(file_meta['run_date'])
+    file_meta['run_time'] = convert_hhmmss(file_meta['run_time'])
+
+    file_meta['is_full_load'] = None
+    file_meta[date_column_name] = datetime.strptime(' '.join([file_meta['run_date'], file_meta['run_time']]), strftime)
     return file_meta
+
+
+
+
+file_path = r'C:\Users\smammadov\packages\Shared\ALBRIDGE\WFS\A632021111001.TXT'
+firm_crd_number = 'WFS'
+cloud_file_history = ''
+
+file_meta = extract_albridge_file_meta(file_path, firm_crd_number, cloud_file_history)
 
 
 
