069e8852318c75f560663f6c783d0e2ce6e6d3b8
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2021-11-29 09:49:42 -0600

    Change MD5 Key length to 1000

--- src/modules/spark_functions.py
+++ src/modules/spark_functions.py
@@ -380,7 +380,7 @@ def add_id_key(table, key_column_names:list):
         return table
  
     coalesce_list = [coalesce(col(c).cast('string'), lit('')) for c in key_column_names]
-    table = table.withColumn(MD5KeyIndicator, concat_ws('_', *coalesce_list)) # TODO: Change this to IDKeyIndicator later when we can add schema change
+    table = table.withColumn(MD5KeyIndicator, concat_ws('_', *coalesce_list).alias(MD5KeyIndicator, metadata={'maxlength': 1000, 'sqltype': 'varchar(1000)'})) # TODO: Change this to IDKeyIndicator later when we can add schema change
     return table
 
 
@@ -395,7 +395,7 @@ def add_md5_key(table, key_column_names:list=[]):
     if not key_column_names:
         key_column_names = table.columns
     coalesce_list = [coalesce(col(c).cast('string'), lit('')) for c in key_column_names]
-    table = table.withColumn(MD5KeyIndicator, md5(concat_ws('_', *coalesce_list)).alias(MD5KeyIndicator, metadata={'maxlength': 300, 'sqltype': 'varchar(300)'}))
+    table = table.withColumn(MD5KeyIndicator, md5(concat_ws('_', *coalesce_list)).alias(MD5KeyIndicator, metadata={'maxlength': 1000, 'sqltype': 'varchar(1000)'}))
     return table
 
 
