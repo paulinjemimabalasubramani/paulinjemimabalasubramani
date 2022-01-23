fd78d3993d1bac54e9731f434499b9d37253f5a2
Chirra, Sunil <schirra@AdvisorGroup.com>
2022-01-22 23:42:09 -0800

    reverting to csv

--- dags/assets_migrate_datastore_albridge_wfs_dag.py
+++ dags/assets_migrate_datastore_albridge_wfs_dag.py
@@ -46,7 +46,7 @@ with DAG(
 
     ASSETS_MIGRATE_DATASTORE_ALBRIDGE_WFS = SparkSubmitOperator(
          task_id = "ASSETS_MIGRATE_DATASTORE_ALBRIDGE_WFS",
-         application = "/usr/local/spark/app/migrate_csv_with_date.py",
+         application = "/usr/local/spark/app/migrate_csv_3.py",
          name = spark_app_name,
          jars = jars,
          conn_id = "spark_default",
