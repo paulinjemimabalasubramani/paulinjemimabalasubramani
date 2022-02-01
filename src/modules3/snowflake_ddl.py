acb91dbcc4b2449e5094f4e2f9f54de0deada2d7
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2022-01-31 18:58:44 -0600

    {{ WAREHOUSE }}_RAW_WH

--- src/modules3/snowflake_ddl.py
+++ src/modules3/snowflake_ddl.py
@@ -614,7 +614,7 @@ def step7(table_name:str):
     task_suffix = '_MERGE_TASK'
     task_name = f'{table_name.upper()}{task_suffix}'.upper()
     stream_name = f'{SCHEMA_NAME}_RAW.{table_name.upper()}{wid.variant_label}{wid.stream_suffix}'
-    warehouse = '{{ ENV }}_RAW_WH' # data_settings.snowflake_warehouse
+    warehouse = '{{ WAREHOUSE }}_RAW_WH' # data_settings.snowflake_warehouse
 
     step = f"""
 {create_or_replace_func('TASK')} {task_name}
