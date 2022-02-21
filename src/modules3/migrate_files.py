630e96dfdedba7db1bcb5c412a7a81f103b194de
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2022-02-21 10:55:11 -0600

    add fix zip_file_path_exists

--- src/modules3/migrate_files.py
+++ src/modules3/migrate_files.py
@@ -462,6 +462,7 @@ def recursive_migrate_all_files(source_path:str, fn_extract_file_meta, additiona
 
         selected_file_paths = sorted(selected_file_paths)
 
+    zip_file_path_exists = True if zip_file_path else False
     for file_path in selected_file_paths:
         file_name = os.path.basename(file_path)
         file_name_noext, file_ext = os.path.splitext(file_name)
@@ -471,22 +472,22 @@ def recursive_migrate_all_files(source_path:str, fn_extract_file_meta, additiona
                 extract_dir = tmpdir
                 logger.info(f'Extracting {file_path} to {extract_dir}')
                 shutil.unpack_archive(filename=file_path, extract_dir=extract_dir, format='zip')
-                zip_file_path1 = zip_file_path if zip_file_path else file_path # to keep original zip file path, rather than the last zip file path
+                zip_file_path = zip_file_path if zip_file_path_exists else file_path # to keep original zip file path, rather than the last zip file path
                 recursive_migrate_all_files(
                     source_path = extract_dir,
                     fn_extract_file_meta = fn_extract_file_meta,
                     additional_file_meta_columns = additional_file_meta_columns,
                     fn_process_file = fn_process_file,
                     fn_get_dtypes = fn_get_dtypes,
-                    zip_file_path = zip_file_path1,
+                    zip_file_path = zip_file_path,
                     selected_file_paths = [],
                     )
-                update_sql_zip_file_path(zip_file_path=zip_file_path1)
+                update_sql_zip_file_path(zip_file_path=zip_file_path)
             continue
 
         migrate_single_file(
             file_path = file_path,
-            zip_file_path = zip_file_path1,
+            zip_file_path = zip_file_path,
             fn_extract_file_meta = fn_extract_file_meta,
             fn_process_file = fn_process_file,
             fn_get_dtypes = fn_get_dtypes,
