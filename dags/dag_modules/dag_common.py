3cbd8e53021326a24dc3609e60aab65499df9bc4
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2022-04-11 11:03:53 -0500

    msteams_webhook

--- dags/dag_modules/dag_common.py
+++ dags/dag_modules/dag_common.py
@@ -7,9 +7,7 @@ Library for common DAG settings and functions
 # %% Import Libraries
 import os
 
-from urllib.parse import quote
-
-from .teams_webhook import MSTeamsWebhookHook
+from .msteams_webhook import on_failure
 
 
 
@@ -21,9 +19,11 @@ executor_cores = 4
 executor_memory = '16G'
 spark_conf = {}
 
-host_ip = os.environ.get(key='HOST_IP', default='')
-port = '8282'
 host_connection_type = 'http'
+host_ip = os.environ.get(key='HOST_IP', default='')
+
+airflow_webserver_port = '8282'
+airflow_webserver_link = f'{host_connection_type}://{host_ip}:{airflow_webserver_port}'
 
 
 
@@ -32,6 +32,7 @@ host_connection_type = 'http'
 default_args = {
     'owner': 'EDIP',
     'depends_on_past': False,
+    'on_failure_callback': on_failure(airflow_webserver_link=airflow_webserver_link),
 }
 
 ingestion_path = '/opt/EDIP/ingestion'
@@ -64,50 +65,6 @@ jars = get_jars(jars_path=jars_path)
 
 
 
-# %% Construct Airflow log url
-
-def get_log_url(context):
-    """
-    Construct Airflow log url
-    """
-    dag_id = context['dag_run'].dag_id
-    task_id = context['task_instance'].task_id
-    execution_date = quote(context['ts'], safe='')
-
-    log_url = f'{host_connection_type}://{host_ip}:{port}/log?dag_id={dag_id}&task_id={task_id}&execution_date={execution_date}'
-
-    return log_url
-
-
-
-# %% Function to run on task failure
-
-def on_failure(context):
-    """
-    Function to run on task failure
-    """
-    dag_id = context['dag_run'].dag_id
-    task_id = context['task_instance'].task_id
-
-    logs_url = get_log_url(context=context)
-
-    teams_notification_hook = MSTeamsWebhookHook(
-        http_conn_id = 'msteams_webhook',
-        message = f'Failed Pipeline: {dag_id} Task: {task_id}',
-        subtitle = 'Pipeline Failure',
-        button_text = 'Logs',
-        button_url = logs_url,
-        theme_color = 'FF0000'
-    )
-    teams_notification_hook.execute()
-
-    title = f'Title {dag_id} - {task_id}'
-    body = title
-
-
-
-default_args = {**default_args,
-    'on_failure_callback': on_failure,
-    }
+# %%
 
 
---
+++ dags/dag_modules/msteams_webhook.py
@@ -0,0 +1,120 @@
+
+# %% Import Libraries
+
+from urllib.parse import quote
+
+from airflow.providers.http.hooks.http import HttpHook
+
+# %% Paramneters
+
+msteams_http_conn_id = 'msteams_webhook'
+
+
+
+# %% Build On Failure Card
+
+def on_failure_card(message:str, subtitle:str, button_url:str, button_text:str='Logs'):
+    """
+    Build On Failure Card
+    """
+    cardjson = f"""
+    {{
+        "@type": "MessageCard",
+        "@context": "http://schema.org/extensions",
+        "themeColor": "00FF00",
+        "summary": "{message}",
+        "sections": [{{
+            "activityTitle": "{message}",
+            "activitySubtitle": "{subtitle}",
+            "markdown": true,
+            "potentialAction": [
+                {{
+                    "@type": "OpenUri",
+                    "name": "{button_text}",
+                    "targets": [
+                        {{ "os": "default", "uri": "{button_url}" }}
+                    ]
+                }}
+            ]
+        }}]
+    }}
+    """
+
+    return cardjson
+
+
+
+# %% Airflow MS Teams Web Hook
+
+def msteams_httphook(http_conn_id:str, data):
+    """
+    Airflow MS Teams Web Hook
+    """
+    httphook = HttpHook(method='POST', http_conn_id=http_conn_id)
+
+    conn = httphook.get_connection()
+    webhook_token = conn.extra_dejson.get('webhook_token', '')
+
+    proxy_url = conn.extra_dejson.get("proxy", '')
+    proxies = {'https': proxy_url} if len(proxy_url) > 5 else {}
+
+    httphook.run(
+        endpoint = webhook_token,
+        data = data,
+        headers = {'Content-type': 'application/json'},
+        extra_options = {
+            'proxies': proxies,
+            'check_response': False,
+            },
+        )
+
+
+
+# %% Construct Airflow log url
+
+def get_log_url(context, airflow_webserver_link):
+    """
+    Construct Airflow log url
+    """
+    dag_id = context['dag_run'].dag_id
+    task_id = context['task_instance'].task_id
+    execution_date = quote(context['ts'], safe='')
+
+    log_url = f'{airflow_webserver_link}/log?dag_id={dag_id}&task_id={task_id}&execution_date={execution_date}'
+
+    return log_url
+
+
+
+# %% Function to run on task failure
+
+def on_failure(airflow_webserver_link):
+    """
+    Function to run on task failure
+    """
+
+    def on_failure_context(context):
+        """
+        Function to run on task failure
+        """
+        dag_id = context['dag_run'].dag_id
+        task_id = context['task_instance'].task_id
+
+        logs_url = get_log_url(context=context, airflow_webserver_link=airflow_webserver_link)
+
+        data = on_failure_card(
+            http_conn_id = msteams_http_conn_id,
+            message = f'Failed: {dag_id} Task: {task_id}',
+            subtitle = airflow_webserver_link,
+            button_url = logs_url,
+            )
+
+        msteams_httphook(http_conn_id=msteams_http_conn_id, data=data)
+
+    return on_failure_context
+
+
+
+# %%
+
+
