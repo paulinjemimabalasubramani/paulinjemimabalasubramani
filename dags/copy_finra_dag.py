c0db88410ad8f1e08568c69aedd144d547368ef5
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2022-04-18 12:08:50 -0500

    copy_finra_dag

---
+++ dags/copy_finra_dag.py
@@ -0,0 +1,46 @@
+# %% Import Libraries
+
+from airflow import DAG
+
+from airflow.operators.bash_operator import BashOperator
+from airflow.utils.dates import days_ago
+
+from dag_modules.dag_common import default_args, src_path, start_pipe, end_pipe
+
+
+
+# %% Pipeline Parameters
+
+pipelinekey = 'COPY_FINRA'
+
+tags = ['SC:FINRA']
+
+schedule_interval = '5 10 * * *' # https://crontab.guru/
+
+
+
+# %% Create DAG
+
+with DAG(
+    dag_id = pipelinekey.lower(),
+    default_args = default_args,
+    description = pipelinekey,
+    schedule_interval = schedule_interval,
+    start_date = days_ago(1),
+    tags = tags,
+    catchup = False,
+) as dag:
+
+    copy_files = BashOperator(
+        task_id = pipelinekey,
+        bash_command = f'python {src_path}/copy_firna_3.py --pipelinekey {pipelinekey}',
+        dag = dag
+    )
+
+    start_pipe(dag) >> copy_files >> end_pipe(dag)
+
+
+
+# %%
+
+
