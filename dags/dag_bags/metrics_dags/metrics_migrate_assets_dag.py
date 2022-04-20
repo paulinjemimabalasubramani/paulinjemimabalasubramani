407b6d0b8c26bbd31d75c77823457449699d6e4c
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2022-04-20 16:31:51 -0500

    dynamic METRICS_MIGRATE_ASSETS dag

---
+++ dags/dag_bags/metrics_dags/metrics_migrate_assets_dag.py
@@ -0,0 +1,57 @@
+# %% Import Libraries
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+from dag_modules.dag_common import default_args, start_pipe, end_pipe, migrate_data, copy_files, delete_files
+
+
+
+# %% Pipeline Parameters
+
+pipelinekey_prefix = 'METRICS_MIGRATE_ASSETS_'
+python_spark_code = 'migrate_csv_with_date_3'
+
+tags = ['DB:Metrics', 'SC:Assets']
+
+firm_map = {
+    'RAA': '0 14 * * *',
+    'SPF': '10 14 * * *',
+    'WFS': '20 14 * * *',
+    }
+
+
+
+# %% Create DAGs per firm
+
+def create_dag(firm_name, schedule_interval):
+    """
+    Create DAG
+    """
+    pipelinekey = pipelinekey_prefix + firm_name
+
+    dag = DAG(
+        dag_id = pipelinekey.lower(),
+        default_args = default_args,
+        description = pipelinekey,
+        schedule_interval = schedule_interval,
+        start_date = days_ago(1),
+        tags = tags,
+        catchup = False,
+        )
+
+    with dag:
+        start_pipe(dag) >> copy_files(dag, pipelinekey) >> migrate_data(dag, pipelinekey, python_spark_code) >> delete_files(dag, pipelinekey) >> end_pipe(dag)
+
+    globals()[dag.safe_dag_id] = dag
+
+
+
+for firm_name, schedule_interval in firm_map.items():
+    create_dag(firm_name=firm_name, schedule_interval=schedule_interval)
+
+
+
+# %%
+
+
