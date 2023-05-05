f387af87022e68fdbf98fbed8ae4f7b98aa0fb6d
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2023-05-05 11:25:00 -0500

    COMP_MIGRATE_CLIENTREVENUE

---
+++ dags/dag_bags/comp_dags/comp_migrate_clientrevenue_dag.py
@@ -0,0 +1,39 @@
+# %% Import Libraries
+
+from airflow import DAG
+from airflow.utils.dates import days_ago
+
+from dag_modules.dag_common import default_args, start_pipe, end_pipe, migrate_data, copy_files, delete_files
+
+
+
+# %% Pipeline Parameters
+
+pipelinekey = 'COMP_MIGRATE_CLIENTREVENUE'
+python_spark_code = 'migrate_csv_3'
+
+tags = ['DB:Comp', 'SC:ClientRevenue']
+
+schedule_interval = '40 12 * * *' # https://crontab.guru/
+
+
+
+# %% Create DAG
+
+with DAG(
+    dag_id = pipelinekey.lower(),
+    default_args = default_args,
+    description = pipelinekey,
+    schedule_interval = schedule_interval,
+    start_date = days_ago(1),
+    tags = tags,
+    catchup = False,
+) as dag:
+
+    start_pipe(dag) >> copy_files(dag, pipelinekey) >> migrate_data(dag, pipelinekey, python_spark_code) >> delete_files(dag, pipelinekey) >> end_pipe(dag)
+
+
+
+# %%
+
+
