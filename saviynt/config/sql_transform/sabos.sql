12ffe7f30abcea5725030686db27e6c3b5d0e74c
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2024-01-31 17:59:58 -0600

    update pershing and sabos sql

--- saviynt/config/sql_transform/sabos.sql
+++ saviynt/config/sql_transform/sabos.sql
@@ -1,9 +1,9 @@
 CREATE VIEW [dbo].[sabos_accounts_entitlements] AS
 SELECT
-    CONCAT(appdes, ' on ', usrapp) appdes_on_usrapp,
-    REPLACE(emailtrim, '@ADVISORGROUP.COM', '@OSAIC.COM') AS osaic_email,
+    CONCAT(app_description, ' on ', user_application) appdes_on_usrapp,
+    REPLACE(email_id, '@ADVISORGROUP.COM', '@OSAIC.COM') AS osaic_email,
     *
-FROM [dbo].[sabos_useraccess]
+FROM [dbo].[sabos_sabos_access_reports]
 WHERE [meta_is_current]=1
-    AND upstat='*ENABLED'
+    AND status='*ENABLED'
 ;
