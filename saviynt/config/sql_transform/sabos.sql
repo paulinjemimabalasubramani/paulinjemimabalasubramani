474a51639b734e87df78bd5a23cff0c546a87aba
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2024-02-01 15:21:34 -0600

    updated saviynt side of pershing security_profile as full load

--- saviynt/config/sql_transform/sabos.sql
+++ saviynt/config/sql_transform/sabos.sql
@@ -2,7 +2,17 @@ CREATE VIEW [dbo].[sabos_accounts_entitlements] AS
 SELECT
     CONCAT(app_description, ' on ', user_application) appdes_on_usrapp,
     REPLACE(email_id, '@ADVISORGROUP.COM', '@OSAIC.COM') AS osaic_email,
-    *
+    [empid],
+    [user] as [account_name],
+    [name],
+    [email_id],
+    [status],
+    [application],
+    [user_application],
+    [app_description],
+    [security_level],
+    [access],
+    [prodflag]
 FROM [dbo].[sabos_sabos_access_reports]
 WHERE [meta_is_current]=1
     AND status='*ENABLED'
