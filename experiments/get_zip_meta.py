3bf723626e30b0ae34b4138c14ce225fd68a148e
Seymur Mammadov <seymur.mammadov@advisorgroup.com>
2024-02-13 21:09:11 -0600

    azure_filesystem_uri = 'privatelink.dfs.osaic-azure.osaic.com'

--- experiments/get_zip_meta.py
+++ experiments/get_zip_meta.py
@@ -9,19 +9,19 @@ import os
 #zip_file = r'/opt/EDIP/remote/fasoma05bprd/DownloadData/_TRI/2023/June.zip'
 #zip_file = r'C:\myworkdir\temp\temp.zip'
 
-source_path = r'/opt/EDIP/nfs_history_load'
+source_path = r'/opt/EDIP/data/nfs_history_load'
 
 
 # %%
 
 
-for month in ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct']:
+for month in ['Aug', 'Sep', 'Oct']: # ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct']:
     zip_file = rf'/opt/EDIP/remote/fasoma05bprd/DownloadData/_TRI/2023/{month}.zip'
     print('\n'+zip_file)
     with ZipFile(zip_file, 'r') as zipobj:
         zipinfo_list = zipobj.infolist()
         for zipinfo in zipinfo_list:
-            if '_NFS_BOOK_' in zipinfo.filename.upper():
+            if '_NAME' in zipinfo.filename.upper():
                 print(f'{zipinfo.file_size/1024:9.3f} {zipinfo.filename}')
 
                 zipobj.extract(member=zipinfo, path=source_path)
