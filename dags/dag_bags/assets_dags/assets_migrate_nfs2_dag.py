5af98592b9478f7047ddd84ea443471784acd906
abasavarajappa <ankitha.basavarajappa@osaic.com>
2024-10-03 15:59:16 +0530

    latest file

--- dags/dag_bags/assets_dags/assets_migrate_nfs2_dag.py
+++ dags/dag_bags/assets_dags/assets_migrate_nfs2_dag.py
@@ -24,9 +24,9 @@ firm_map = {
     'WFS': '53 */2 * * *',
     'OFS': '57 */2 * * *',
     'LFA_DAILY': '57 */2 * * *',
-    'LFA_WEEKLY': '57 */2 * * *',
+    'LFA_FULL': '0 10 * * 6',
     'LFS_DAILY': '57 */2 * * *',
-    'LFS_WEEKLY': '57 */2 * * *',
+    'LFS_FULL': '0 10 * * 6',
     'HISTORY': None,
     }
 
