003dfcbbde5e65c608bd7047ef06410c277ddce6
mrangaswamy <mageshbabu.rangaswamy@osaic.com>
2025-04-14 10:46:52 -0400

    renamed NFS asset transfer table name

--- src/nfs2_to_json_3.py
+++ src/nfs2_to_json_3.py
@@ -309,7 +309,7 @@ def extract_values_from_line(line:str, record_schema:list,line_number:str=None,f
         field_values['file_line_number'] = line_number
 
     if file_meta:        
-        if any(file_meta['table_name_no_firm'] in item for item in ['transfer_summary','transfer_detail']):
+        if any(file_meta['table_name_no_firm'] in item for item in ['asset_transfer_summary','asset_transfer_detail']):
             field_values['transmission_creation_date'] = file_meta['transmission_creation_date']
     
     return field_values
@@ -346,11 +346,11 @@ def process_lines_1_record(fsource, ftarget, file_meta:dict):
 
     first = True
        
-    add_file_line_numbers_to_table = ['bookkeeping','transfer_summary','transfer_detail']
+    add_file_line_numbers_to_table = ['bookkeeping','asset_transfer_summary','asset_transfer_detail']
     
     line_number = 0 if any(file_meta['table_name_no_firm'] in item for item in add_file_line_numbers_to_table) else None
     
-    if file_meta['table_name'] not in ['raa_transfer_summary','raa_transfer_detail']:
+    if file_meta['table_name'] not in ['raa_asset_transfer_summary','raa_asset_transfer_detail']:
         valid_line_start_chars = ['D']
     else:
         valid_line_start_chars = ['1','2']
@@ -596,8 +596,8 @@ def process_lines_user_id_administration(fsource, ftarget, file_meta:dict):
 # %%
 
 process_lines_map = {
-    'transfer_summary': process_lines_1_record,
-    'transfer_detail': process_lines_1_record,
+    'asset_transfer_summary': process_lines_1_record,
+    'asset_transfer_detail': process_lines_1_record,
     'bookkeeping': process_lines_1_record,
     'bookkeeping': process_lines_1_record,
     'bookkeeping_iws': process_lines_1_record,
