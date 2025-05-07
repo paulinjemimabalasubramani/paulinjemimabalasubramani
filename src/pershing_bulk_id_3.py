68da6d56a116f3f48844cf4bff544d7bae654747
mrangaswamy <mageshbabu.rangaswamy@osaic.com>
2025-05-07 16:00:57 -0400

    code refactor

--- src/pershing_bulk_id_3.py
+++ src/pershing_bulk_id_3.py
@@ -95,11 +95,11 @@ def lines_to_hex(ftarget, lines:list,date_of_data:str=None):
     if len(lines) == 0: return
 
     hash = hash_func()
+
     for line in lines:
-        if date_of_data:
-            hash.update(line+date_of_data.strftime(r"%Y-%m-%d %H:%M:%S").encode('utf-8'))
-        else:
-            hash.update(line.encode('utf-8'))
+        hash_line= line+date_of_data.strftime(r"%Y-%m-%d %H:%M:%S") if date_of_data else line
+        hash.update(hash_line.encode('utf-8'))
+
     hex = hash.hexdigest()
 
     for line in lines:
